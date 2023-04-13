44f5ebd8149739cc6f537045603c2e2badd1b8d8
vadirajkulkarnisheshadri <85036297+Vadirajvkulkarnisheshadri@users.noreply.github.com>
2023-04-13 17:32:14 +0530

    js syntax

--- js-syntax.js
+++ js-syntax.js
@@ -1,9 +1,5 @@
-// Creating a Variables in JavaScript
-var x;
-var y;
 
-// How to use variables:
-x = 75;
-y = 1000;
-let z = x + y;
-console.log(z);   
+// <script ...>
+// JavaScript code
+// </script>
+
